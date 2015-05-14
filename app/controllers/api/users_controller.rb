608457ec4e5abf5140a3a10ad38948344578a78d
Alex Haas <alex.carl.haas@gmail.com>
2015-05-14 11:13:26 -0400

    Add UserShow Jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,7 +17,7 @@ class Api::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: @user
+    render :show
   end
 
   def index
---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,4 @@
+json.id @user.id
+json.username @user.username
+json.email @user.email
+json.songs @user.songs
