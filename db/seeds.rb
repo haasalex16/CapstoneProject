9692275e77198e3701ecd9e974438efad7371775
Alex Haas <alex.carl.haas@gmail.com>
2015-05-14 09:13:37 -0400

    Add Basic Seedings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+a = User.create(username: 'Alex Haas', email: 'alexhaas@email.com', password: 'password')
+b = User.create(username: 'Example', email: 'example@email.com', password: 'password')
+c = User.create(username: 'Steve Holt', email: 'holt@email.com', password: 'password')
+d = User.create(username: 'Mike Bluth', email: 'mbluth@email.com', password: 'password')
 
-# User.create({username: 'Alex Haas', email: 'alexhaas@email.com', password: 'password'})
+a.songs.create(title: 'DMB - Crash', upload_id: '24rnfjfr')
+a.songs.create(title: 'Josh Cocktail - Fire', upload_id: '24rnfjfr')
+a.songs.create(title: 'The Neighbourhood - Sweater Weather', upload_id: '24rnfjfr')
+a.songs.create(title: 'Luke Bryan - Crash My Party', upload_id: '24rnfjfr')
+a.songs.create(title: 'The Deans List - Caveman', upload_id: '24rnfjfr')
+
+b.songs.create(title: 'Jack Johnson - Holes to Heaven', upload_id: '24rnfjfr')
+b.songs.create(title: 'Mike Posner - Wonderwall [cover]', upload_id: '24rnfjfr')
+b.songs.create(title: 'KiD CuDi - CudderIsBack', upload_id: '24rnfjfr')
+
+c.songs.create(title: 'The Fray - Over My Head', upload_id: '24rnfjfr')
+c.songs.create(title: 'Nappy Roots - AwwNaw', upload_id: '24rnfjfr')
+c.songs.create(title: 'Wolfgang Gartner - Space Junk', upload_id: '24rnfjfr')
+
+d.songs.create(title: 'Daddys Groove - Hurricane', upload_id: '24rnfjfr')
+d.songs.create(title: 'Nosaj Thing - Aquarium', upload_id: '24rnfjfr')
+d.songs.create(title: 'Big Sean - Marvin & Chardonnay (feat. Kanye West)', upload_id: '24rnfjfr')
