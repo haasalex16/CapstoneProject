ed8b50272ad8afe8d4635df6a383673d3482263d
Alex Haas <Alex.carl.Haas@gmail.com>
2015-05-22 15:33:28 -0400

    Presentations

--- app/assets/javascripts/views/playlists/playlist_show.js
+++ app/assets/javascripts/views/playlists/playlist_show.js
@@ -3,7 +3,7 @@ EclecticEar.Views.PlaylistShow = Backbone.CompositeView.extend ({
   className: 'playlist-show group',
 
   initialize: function() {
-    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'sync', this.render);
   },
 
   events: {
