1a9c232fbec99d2155e573e0461ea8be58fb9f64
Alex Haas <alex.carl.haas@gmail.com>
2015-06-04 20:18:39 -0400

    Add music presence validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :title, presence: true
-  # validates_attachment_presence :music
+  validates :music, :attachment_presence => true
 
   belongs_to :user
   has_many :taggings
