4505ec3bdefddde29137d7b56933b5fde066211f
Alex Haas <alex.carl.haas@gmail.com>
2015-06-06 00:02:21 -0400

    fix autoplay transitions

--- app/assets/javascripts/views/home/media.js
+++ app/assets/javascripts/views/home/media.js
@@ -34,12 +34,14 @@ EclecticEar.Views.Media = Backbone.CompositeView.extend ({
 
   nextSong: function() {
     $(this.items[this.idx]).find('.song-show').removeClass('song-show').addClass('song-show-list');
-    $(this.items[this.idx]).find('.stop-button').removeClass('stop-button').addClass('play-button').removeClass('fadein');
+    $(this.items[this.idx]).find('.stop-button').removeClass('stop-button').addClass('play-button');
+    $(this.items[this.idx]).find('.fa-stop').removeClass('fa-stop').addClass('fa-play').removeClass('fadein');
     $(this.items[this.idx]).find('.album-art').addClass('album-art-list').removeClass('album-art');
     $(this.items[this.idx]).find('.top-row').addClass('top-row-list').removeClass('top-row');
     $(this.items[this.idx]).find('.small-stop-button').removeClass('small-stop-button').addClass('small-play-button');
     this.idx++;
-    $(this.items[this.idx]).find('.play-button').removeClass('play-button').addClass('stop-button').addClass('fadein');
+    $(this.items[this.idx]).find('.play-button').removeClass('play-button').addClass('stop-button');
+    $(this.items[this.idx]).find('.fa-play').removeClass('fa-play').addClass('fa-stop').addClass('fadein');
     $(this.items[this.idx]).find('.album-art-list').addClass('album-art').removeClass('album-art-list');
     $(this.items[this.idx]).find('.top-row-list').addClass('top-row').removeClass('top-row-list');
     $(this.items[this.idx]).find('.song-show-list').removeClass('song-show-list').addClass('song-show');
